fbcb69ac77c5a75ba74966a472d4cef7782da7be
Raditya Ananda Rohman <anandarohman2@gmail.com>
2025-01-13 07:38:48 +0700

    update hero and about section

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+const navbar = document.getElementById('navbar');
+const navlink = document.querySelectorAll('.navbar-link');
+
+// Toggle Menu Visibility & ARIA Attribute
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('active'); // Menambahkan animasi slide-in
+
+  const expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
+  menuButton.setAttribute('aria-expanded', !expanded);
+});
+
+// Menambahkan efek blur pada navbar saat scroll
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add('backdrop-blur-lg', 'shadow-md');
+    navlink.classList.add('text-yellow-light')
+  } else {
+    navbar.classList.remove('backdrop-blur-lg', 'shadow-md');
+    navlink.classList.remove('text-yellow-light')
+  }
+});
