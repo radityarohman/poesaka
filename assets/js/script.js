96496e1f78d03f8ad4fa78fd3164e1eba3c11dcf
Raditya Ananda Rohman <anandarohman2@gmail.com>
2025-01-21 18:49:41 +0700

    delete unnecesarry files and code refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
 const navbar = document.getElementById('navbar');
-const navlink = document.querySelectorAll('nav a');
 
 // Toggle Menu Visibility & ARIA Attribute
 menuButton.addEventListener('click', () => {
@@ -16,33 +15,7 @@ menuButton.addEventListener('click', () => {
 window.addEventListener('scroll', () => {
   if (window.scrollY > 10) {
     navbar.classList.add('backdrop-blur-lg', 'shadow-md');
-    navlink.classList.add('text-yellow-light')
   } else {
     navbar.classList.remove('backdrop-blur-lg', 'shadow-md');
-    navlink.classList.remove('text-yellow-light')
   }
-});
-
-// Mengatur posisi anchor link saat diklik (scroll offset)
-navLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault(); // Mencegah perilaku default
-
-    const targetId = link.getAttribute('href').substring(1); // Menghapus tanda #
-    const targetElement = document.getElementById(targetId);
-
-    if (targetElement) {
-      const offset = navbar.offsetHeight; // Menghitung tinggi navbar
-      const targetPosition = targetElement.offsetTop - offset;
-
-      window.scrollTo({
-        top: targetPosition,
-        behavior: 'smooth',
-      });
-
-      // Menutup menu (untuk perangkat mobile)
-      menu.classList.add('hidden');
-      menuButton.setAttribute('aria-expanded', false);
-    }
-  });
 });
