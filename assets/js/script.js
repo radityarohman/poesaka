84890819a95fa64c7ad6ce8dd5f452472af7e1cd
Raditya Ananda Rohman <anandarohman2@gmail.com>
2025-01-22 14:50:18 +0700

    update responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,23 @@ const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
 const navbar = document.getElementById('navbar');
 
-// Toggle Menu Visibility & ARIA Attribute
 menuButton.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-  menu.classList.toggle('active'); // Menambahkan animasi slide-in
-
-  const expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
+  const expanded = menuButton.getAttribute('aria-expanded') === 'true';
   menuButton.setAttribute('aria-expanded', !expanded);
+
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+    // Trigger reflow to ensure animation runs
+    menu.offsetHeight; 
+    menu.classList.add('active');
+  } else {
+    menu.classList.remove('active');
+    setTimeout(() => {
+      menu.classList.add('hidden');
+    }, 500); // Match this duration with the CSS transition
+  }
 });
+
 
 // Menambahkan efek blur pada navbar saat scroll
 window.addEventListener('scroll', () => {
